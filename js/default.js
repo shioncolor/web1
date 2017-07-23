a22f1465d2a1c7064edbdcd26ad98c89f594c172
shioncolor <czamy00@gmail.com>
2017-07-24 04:12:37 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -8,6 +8,7 @@ var separate_time=function(time){
  return[sec,min,hours,days,month,year];
 }
 
+var update=function(){
 var now = new Date();
 var counter=separate_time(now);
 document.getElementById('countdown').textContent =
@@ -17,3 +18,10 @@ document.getElementById('countdown').textContent =
   counter[2]+'時'+
   counter[1]+'分'+
   counter[0]+'秒';
+refresh();
+}
+var refresh=function()
+{
+setTimeout(update,1000);
+}
+update();
